61720fd82b6f551b4ac38e200c3d55cbecc1f656
amateurmaker <34817364+amateurmaker@users.noreply.github.com>
2021-12-27 22:56:34 +0800

    Update dayOne.py
    Added part 2 of the day one code

--- advent_of_code/dayOne.py
+++ advent_of_code/dayOne.py
@@ -1,4 +1,5 @@
 #! /usr/bin/env/ python3
+import copy
 
 print("Day one challenge")
 
@@ -18,5 +19,49 @@ for i in range(1, len(Lines)):
 
     # Update the previous number 
     prevNum = line
+
+print("The answer is {}".format(count))
+
+
+print("Day one part 2 of the challenge")
+
+first_window = []
+second_window = []
+third_window = []
+
+file = open('dayone.txt', 'r')
+Lines = file.readlines()
+
+first_window.append(int(Lines[0]))
+first_window.append(int(Lines[1]))
+first_window.append(int(Lines[2]))
+
+second_window.append(int(Lines[1]))
+second_window.append(int(Lines[2]))
+second_window.append(int(Lines[3]))
+
+third_window.append(int(Lines[2]))
+third_window.append(int(Lines[3]))
+
+if (sum(second_window) > sum(first_window) > 0):
+    count =1
+else:
+    count = 0
+
+for i in range(4, len(Lines)):
+    line = int(Lines[i])
+
+    second_window.append(line)
+    third_window.append(line)
+
+    if ((sum(second_window)) - (sum(first_window)) > 0):
+       count += 1
+
+    first_window = copy.deepcopy(second_window)
+    second_window = copy.deepcopy(third_window)
+
+    temp_var = third_window[1]
+    third_window.clear()
+    third_window.append(temp_var)
 
 print("The answer is {}".format(count))
