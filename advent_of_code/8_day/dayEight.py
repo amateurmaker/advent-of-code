58e6927f2bd83100ce0a1985091559472e48ef1e
Denesh <Denesh@Users-MacBook-Pro.local>
2022-02-02 23:10:17 +0800

    Finished day 8 with the help of https://github.com/jeff-fred/Advent-of-Code-2021/blob/main/Day8/day8.py

--- advent_of_code/8_day/dayEight.py
+++ advent_of_code/8_day/dayEight.py
@@ -67,6 +67,7 @@ new_list = []
 for line in Lines:
     new_list.append(stripNewLine(convertString(line))) 
 
+
 count = 0
 for element in new_list:
     for i in range(4):
@@ -79,6 +80,69 @@ for element in new_list:
         elif (countDis(element[-i - 1]) == 7):
             count += 1
 print(count)
+
+
+
+def part2():
+    inputs = [line.split('|')[0].strip().split(' ') for line in Lines]
+    outputs = [line.split('|')[1].strip().split(' ') for line in Lines]
+    totalSum = 0
+
+    for i in range(len(Lines)):
+        digitMap = {}
+
+        # Update hash with known digits
+        for digit in inputs[i]:
+            if len(digit) == 2:
+                digitMap[1] = digit
+            elif len(digit) == 4:
+                digitMap[4] = digit
+            elif len(digit) == 3:
+                digitMap[7] = digit
+            elif len(digit) == 7:
+                digitMap[8] = digit
+
+        # Check for unknown digits using the issubset method from the type set
+        for digit in inputs[i]:
+            # For digits 0, 6, and 9 
+            if len(digit) == 6:
+                # 4 is part of 9 
+                if set(digitMap[4]).issubset((digit)):
+                    digitMap[9] = digit
+                
+                # 1 is part of 0
+                elif set(digitMap[1]).issubset((digit)):
+                    digitMap[0] = digit
+
+                else:
+                    digitMap[6] = digit
+                
+        # Check for unknown digits
+        for digit in inputs[i]:
+            # For digits 2, 3, and 5
+            if len(digit) == 5:
+                if set(digit).issubset((digitMap[6])):
+                    digitMap[5] = digit
+                elif set(digitMap[1]).issubset((digit)):
+                    digitMap[3] = digit
+                else:
+                    digitMap[2] = digit
+
+        number = []
+
+        # Decode the outputs
+        for digit in outputs[i]:
+            for key, value in digitMap.items():
+                # Converting to set is necessary or else the strings don't equal to each other
+                if set(digit) == set(value):
+                    number.append(str(key))
+                    
+        number = int(''.join(number))
+        totalSum += number
+    
+    return totalSum
+
+print(f"The second answer is: {part2()}")
 
 # Extra code that could be useful
 # oxy_Lines_prev = copy.deepcopy(Lines)
