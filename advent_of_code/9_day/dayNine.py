ab12a3b9d36d990103fb95941a52f10bdd5a9dea
Denesh <Denesh@Users-MacBook-Pro.local>
2022-02-03 01:59:23 +0800

    Finished up day nine

--- advent_of_code/9_day/dayNine.py
+++ advent_of_code/9_day/dayNine.py
@@ -73,6 +73,7 @@ last_x = len(new_new_list) - 1
 last_y = len(new_new_list[0]) - 1
 # print(f"The last x is: {last_x} and the last y is: {last_y}")
 count = 0
+count_list = []
 for i in range(len(new_new_list)):
     for j in range(len(new_new_list[i])):
         number = new_new_list[i][j]
@@ -80,53 +81,61 @@ for i in range(len(new_new_list)):
             if (number < new_new_list[i + 1][j] and number < new_new_list[i][j + 1]):
                 # print(f"This number is the lowest: {number}")
                 count += int(number) + 1
+                count_list.append([i, j])
             # print("top left corner")
         elif (i == last_x and j == 0):
             if (number < new_new_list[i - 1][j] and number < new_new_list[i][j - 1]):
                 # print(f"This number is the lowest: {number}")
                 count += int(number) + 1
+                count_list.append([i, j])
             # print("top right corner")
         elif (i == last_x and j == last_y):
             if (number < new_new_list[i - 1][j] and number < new_new_list[i][j - 1]):
                 # print(f"This number is the lowest: {number}")
                 count += int(number) + 1
+                count_list.append([i, j])
             # print("bottom right")
         elif (i == 0 and j == last_y):
             if (number < new_new_list[i - 1][j] and number < new_new_list[i][j - 1]):
                 # print(f"This number is the lowest: {number}")
                 count += int(number) + 1
+                count_list.append([i, j])
             # print("bottom left")
         elif (i == 0):
             if (number < new_new_list[i][j - 1] and number < new_new_list[i][j + 1] and number < new_new_list[i+1][j]):
                 # print(f"This number is the lowest: {number}")
                 count += int(number) + 1
+                count_list.append([i, j])
             # print("top wall")
         elif (i == last_x):
             if (number < new_new_list[i][j - 1] and number < new_new_list[i][j + 1] and number < new_new_list[i-1][j]):
                 # print(f"This number is the lowest: {number}")
                 count += int(number) + 1
+                count_list.append([i, j])
             # print("bottom wall")
         elif (j == last_y):
             if (number < new_new_list[i - 1][j] and number < new_new_list[i + 1][j] and number < new_new_list[i][j - 1]):
                 # print(f"This number is the lowest: {number}")
                 count += int(number) + 1
+                count_list.append([i, j])
             # print("right wall")
         elif (j == 0):
             if (number < new_new_list[i - 1][j] and number < new_new_list[i + 1][j] and number < new_new_list[i][j + 1]):
                 # print(f"This number is the lowest: {number}")
                 count += int(number) + 1
+                count_list.append([i, j])
         else:
             if (number < new_new_list[i - 1][j] and number < new_new_list[i + 1][j] and number < new_new_list[i][j - 1] and number < new_new_list[i][j + 1]):
                 # print(f"This number is the lowest: {number}")
                 count += int(number) + 1
+                count_list.append([i, j])
             # print("left wall")
 
 print(f"the part 1 number is: {count}")
 
 
-def checkadjascent(i, j):
+def checkadjascent(i, j, num):
     global new_new_list
-    answer = 0
 
     if (i < 0 or i > last_x):
         return 0
@@ -142,55 +151,58 @@ def checkadjascent(i, j):
     if (int(new_new_list[i][j]) == 9):
         return 0
 
+    if (int(num) > int(new_new_list[i][j])):
+        return 0
+
     # print(f"the index is: {i} and {j} with the number {new_new_list[i][j]}")
     
     # mark is as checked for future iterations
     new_new_list[i][j] = 100
     
-    answer += 1
+    answer = 1
     if (i == 0):
         if (j == 0):
-            answer += checkadjascent(i + 1, j)
-            answer += checkadjascent(i, j + 1)
+            answer += checkadjascent(i + 1, j, num)
+            answer += checkadjascent(i, j + 1, num)
         elif (j == last_y):
-            answer += checkadjascent(i - 1, j)
-            answer += checkadjascent(i, j + 1)
+            answer += checkadjascent(i - 1, j, num)
+            answer += checkadjascent(i, j + 1, num)
         else:
-            answer += checkadjascent(i - 1, j)
-            answer += checkadjascent(i + 1, j)
-            answer += checkadjascent(i, j + 1)
+            answer += checkadjascent(i - 1, j, num)
+            answer += checkadjascent(i + 1, j, num)
+            answer += checkadjascent(i, j + 1, num)
     elif (i == last_x):
         if (j == 0):
-            answer += checkadjascent(i - 1, j)
-            answer += checkadjascent(i, j + 1)
+            answer += checkadjascent(i - 1, j, num)
+            answer += checkadjascent(i, j + 1, num)
         elif (j == last_y):
-            answer += checkadjascent(i - 1, j)
-            answer += checkadjascent(i, j - 1)
+            answer += checkadjascent(i - 1, j, num)
+            answer += checkadjascent(i, j - 1, num)
         else:
-            answer += checkadjascent(i - 1, j)
-            answer += checkadjascent(i, j - 1)
-            answer += checkadjascent(i, j + 1)
+            answer += checkadjascent(i - 1, j, num)
+            answer += checkadjascent(i, j - 1, num)
+            answer += checkadjascent(i, j + 1, num)
     else:
-        answer += checkadjascent(i + 1, j)
-        answer += checkadjascent(i - 1, j)
-        answer += checkadjascent(i, j - 1)
-        answer += checkadjascent(i, j + 1)
+        answer += checkadjascent(i + 1, j, num)
+        answer += checkadjascent(i - 1, j, num)
+        answer += checkadjascent(i, j - 1, num)
+        answer += checkadjascent(i, j + 1, num)
 
     return answer
 
 
 
 count = []
-for i in range(len(new_new_list)):
-    for j in range(len(new_new_list[i])):
-        number = new_new_list[i][j]
-        if (number != 9 and number != 100):
-            num = checkadjascent(i, j)
-            if (num != 0):
-                count.append(int(num))
+for stuff in count_list:  
+    i = stuff[0]  
+    j = stuff[1]
+    number = new_new_list[i][j]
+    if (number != 9 and number != 100):
+        num = checkadjascent(i, j, number)
+        if (num != 0):
+            count.append(int(num))
 
 max_three = [count[0], count[0], count[0]]
-print(count)
 for thing in count:
     if (thing > max_three[2]):
         max_three[0] = max_three[1]
@@ -201,17 +213,17 @@ for thing in count:
     if (thing > max_three[1]):
         max_three[0] = max_three[1]
         max_three[1] = thing
+        continue
 
     if (thing > max_three[0]):
         max_three[0] = thing
 
-    
+# print(max_three)
 ans = 1
-print(max_three)
 for thing in max_three:
     ans *= thing
 
-print(f"the part two answer is: {ans}")
+print(f"the part 2 answer is: {ans}")
 
 # Extra code that could be useful
 # oxy_Lines_prev = copy.deepcopy(Lines)
